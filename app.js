9a70864d951cffa0daa8a499c0e0c4085402c328
Pavel Uzuniants <holabeda@gmail.com>
2023-06-22 19:40:35 +0300

    feat: Добавил CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express"
 import mongoose from "mongoose"
+import cors from "cors"
 import { limiter } from "./utils/rateLimiter.js"
 import dotenv from "dotenv"
 import { router } from "./routes/index.js"
 import celebrate from "celebrate"
+import { corsOptions } from "./middlewares/corsOptions.js"
 import { PORT, DATA_BASE } from "./config.js"
 import { requestLogger, errorLogger } from "./middlewares/logger.js"
 import { handleErrors } from "./middlewares/validate.js"
@@ -18,6 +20,7 @@ app.use(express.urlencoded({ extended: true }))
 mongoose.connect(DATA_BASE)
 
 app.use(requestLogger)
+app.use(cors(corsOptions))
 app.use(router)
 app.use(errorLogger)
 app.use(celebrate.errors())
