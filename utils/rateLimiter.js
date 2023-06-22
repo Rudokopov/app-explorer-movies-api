922d5a73b360ccf3bc83338396e45f25f4755f50
Pavel Uzuniants <holabeda@gmail.com>
2023-06-22 20:52:42 +0300

    feat: Прибавил колличество запросов на сервер

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit"
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 3,
+  max: 350,
   message:
     "Превышено количество запросов. Пожалуйста, повторите попытку позже.",
 })
